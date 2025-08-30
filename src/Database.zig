d445518b3344766a80f7744820d8887a335a6f37
Komo <afiqquraisyzulkarnain@gmail.com>
2025-08-31 05:50:28 +0800

    partial

--- src/Database.zig
+++ src/Database.zig
@@ -6,27 +6,38 @@ const StringCapability = capabilities.StringCapability;
 const std = @import("std");
 const mem = std.mem;
 
-const bool_caps_len = @typeInfo(BooleanCapability).@"enum".fields.len;
-const num_caps_len = @typeInfo(NumberCapability).@"enum".fields.len;
-const str_caps_len = @typeInfo(StringCapability).@"enum".fields.len;
+const max_bool_caps = @typeInfo(BooleanCapability).@"enum".fields.len;
+const max_num_caps = @typeInfo(NumberCapability).@"enum".fields.len;
+const max_string_caps = @typeInfo(StringCapability).@"enum".fields.len;
 
-names: [17]?[]const u8,
-description: []const u8 = "",
+name: []const u8,
+aliases: [][]const u8,
+description: []const u8,
 
-_bool_caps: [bool_caps_len]bool = mem.zeroes([bool_caps_len]bool),
-_num_caps: [num_caps_len]?i32 = mem.zeroes([num_caps_len]?i32),
-_string_caps: [str_caps_len]?[]const u8 = mem.zeroes([str_caps_len]?[]const u8),
+allocator: mem.Allocator,
+
+bool_caps: [max_bool_caps]bool = mem.zeroes([max_bool_caps]bool),
+num_cups: [max_num_caps]?i32 = mem.zeroes([max_num_caps]?i32),
+string_caps: [max_string_caps]?[]const u8 = mem.zeroes([max_string_caps]?[]const u8),
+
+bool_ext_caps: std.StringHashMap(bool) = undefined,
+num_ext_caps: std.StringHashMap(i32) = undefined,
+string_ext_caps: std.StringHashMap([]const u8) = undefined,
 
 const Self = @This();
 
+pub fn deinit(self: Self) void {
+    self.allocator.free(self.aliases);
+}
+
 pub fn getBooleanCapability(self: Self, cap: BooleanCapability) bool {
-    return self._bool_caps[@intFromEnum(cap)];
+    return self.bool_caps[@intFromEnum(cap)];
 }
 
 pub fn getNumberCapability(self: Self, cap: NumberCapability) ?i32 {
-    return self._num_caps[@intFromEnum(cap)];
+    return self.num_cups[@intFromEnum(cap)];
 }
 
 pub fn getStringCapability(self: Self, cap: StringCapability) ?[]const u8 {
-    return self._string_caps[@intFromEnum(cap)];
+    return self.string_caps[@intFromEnum(cap)];
 }
--- src/parser.zig
+++ src/parser.zig
@@ -8,18 +8,18 @@ const StringCapability = capabilities.StringCapability;
 const std = @import("std");
 const mem = std.mem;
 
-const bool_caps_len = @typeInfo(BooleanCapability).@"enum".fields.len;
-const num_caps_len = @typeInfo(NumberCapability).@"enum".fields.len;
-const str_caps_len = @typeInfo(StringCapability).@"enum".fields.len;
+const max_bool_caps = @typeInfo(BooleanCapability).@"enum".fields.len;
+const max_num_caps = @typeInfo(NumberCapability).@"enum".fields.len;
+const max_string_caps = @typeInfo(StringCapability).@"enum".fields.len;
 
 const NumType = enum {
     i16,
     i32,
 };
 
-pub const Error = error{ NotATerminfo, ParsingFailed, AdditionalCapabilities };
+pub const Error = error{ NotATerminfo, ParsingFailed, AdditionalCapabilities } || mem.Allocator.Error;
 
-pub fn parse(input: []const u8) Error!Database {
+pub fn parse(allocator: mem.Allocator, input: []const u8) Error!Database {
     var start: usize = 0;
 
     const magic = try readI16(input, &start);
@@ -48,64 +48,64 @@ pub fn parse(input: []const u8) Error!Database {
 
     const table = try readN(input, &start, table_size);
 
-    _ = .{ offsets, table };
-
-    var db = Database{
-        .names = mem.zeroes([17]?[]const u8),
-    };
+    var name: []const u8 = undefined;
+    var description: []const u8 = undefined;
+    var aliases: [][]const u8 = undefined;
 
     {
         var it = mem.splitScalar(u8, names[0 .. names.len - 1], '|');
-        var names_list = mem.zeroes([17]?[]const u8);
-        names_list[0] = it.first();
+        name = it.first();
 
         if (it.peek() != null) {
-            var i: usize = 1;
-            while (it.peek() != null and i < 17) : (i += 1) {
-                const name = it.next().?;
-                names_list[i] = name;
+            var names_list = std.ArrayList([]const u8).empty;
+            while (it.next()) |alias| {
+                try names_list.append(allocator, alias);
             }
-            db.description = names_list[i - 1].?;
-            names_list[i - 1] = null;
-            db.names = names_list;
+            description = names_list.pop().?;
+            aliases = try names_list.toOwnedSlice(allocator);
         }
     }
 
-    if (bool_count > bool_caps_len)
+    const bool_caps = if (bool_count > max_bool_caps)
         return error.AdditionalCapabilities
-    else {
+    else blk: {
+        var caps = mem.zeroes([max_bool_caps]bool);
         start = 0;
         for (0..bool_count - 1) |i| {
             const num = try readI8(booleans, &start);
-            db._bool_caps[i] = switch (num) {
+            caps[i] = switch (num) {
                 -1, 0 => false,
                 else => true,
             };
         }
-    }
+        break :blk caps;
+    };
 
-    if (num_count > num_caps_len)
+    const num_caps = if (num_count > max_num_caps)
         return error.AdditionalCapabilities
-    else {
+    else blk: {
+        var caps = mem.zeroes([max_num_caps]?i32);
         start = 0;
         for (0..num_count - 1) |i| {
             const num = try readInt(numbers, &start, num_type);
             try assert(num >= -2);
-            db._num_caps[i] = switch (num) {
+            caps[i] = switch (num) {
                 -1, -2 => null,
                 else => num,
             };
         }
-    }
+        break :blk caps;
+    };
 
-    if (offset_count > str_caps_len)
+    const string_caps = if (offset_count > max_string_caps)
         return error.AdditionalCapabilities
-    else {
+    else res: {
+        var caps = mem.zeroes([max_string_caps]?[]const u8);
         var offset_start: usize = 0;
         for (0..offset_count - 1) |i| {
             const offset = try readI16(offsets, &offset_start);
             try assert(offset >= -2);
-            db._string_caps[i] = switch (offset) {
+            caps[i] = switch (offset) {
                 -1, -2 => null,
                 else => blk: {
                     var exact_start: usize = 0;
@@ -115,14 +115,60 @@ pub fn parse(input: []const u8) Error!Database {
                 },
             };
         }
+        break :res caps;
+    };
+
+    if (start == input.len) {
+        if (table_size % 2 != 0)
+            try readN(input, &start, 1);
+
+        const ext_bool_count: usize = @intCast(try readU16(input, &start));
+        const ext_num_count: usize = @intCast(try readU16(input, &start));
+        const ext_string_count: usize = @intCast(try readU16(input, &start));
+        const ext_offset_count: usize = @intCast(try readU16(input, &start));
+        const ext_table_size: usize = @intCast(try readU16(input, &start));
+
+        const ext_booleans = try readN(input, &start, ext_bool_count);
+
+        if (ext_bool_count % 2 != 0)
+            try readN(input, &start, 1);
+
+        const ext_numbers = try readN(input, &start, ext_num_count);
+
+        const ext_offsets = try readN(input, &start, ext_string_count);
+
+        const ext_names_raw = try readN(input, &start, ext_bool_count + ext_num_count + ext_string_count);
+
+        const ext_table = try readN(input, &start, ext_table_size);
+
+        const ext_names = blk: {
+            var arr = std.ArrayList([]const u8).empty;
+            var it = mem.splitScalar(u8, ext_table, 0);
+            while (it.next()) |str| {
+                try arr.append(allocator, str);
+            }
+            break :blk try arr.toOwnedSlice(allocator);
+        };
+        defer allocator.free(ext_names);
+
+        const ext_bools = blk: {
+            var map = std.StringHashMap(bool).init(allocator);
+            start = 0;
+        };
     }
 
-    return db;
+    return Database{
+        .name = name,
+        .description = description,
+        .aliases = aliases,
+        .bool_caps = bool_caps,
+        .num_caps = num_caps,
+        .string_caps = string_caps,
+    };
 }
 
 fn readI8(input: []const u8, start: *usize) !i8 {
     const match = input[start.*..];
-    // std.debug.print("I8: {x}\n", .{match[0]});
     try assert(match.len >= 1);
     start.* += 1;
     return mem.readInt(i8, match[0..1], .little);
@@ -137,7 +183,6 @@ fn readU16(input: []const u8, start: *usize) !u16 {
 
 fn readI16(input: []const u8, start: *usize) !i16 {
     const match = input[start.*..];
-    // std.debug.print("I16: {x} {x}\n", .{ match[0], match[1] });
     try assert(match.len >= 2);
     start.* += 2;
     return mem.readInt(i16, match[0..2], .little);
@@ -160,7 +205,6 @@ fn readInt(input: []const u8, start: *usize, typ: NumType) !i32 {
 fn readN(input: []const u8, start: *usize, n: usize) ![]const u8 {
     const match = input[start.*..];
     try assert(match.len >= n);
-    std.debug.print("input: {} match: {}\n", .{ match.len, match[0..n].len });
     start.* += n;
     return match[0..n];
 }
@@ -174,6 +218,15 @@ fn readUntil(input: []const u8, start: *usize, until: []const u8) ![]const u8 {
     return first.?;
 }
 
+fn find(comptime T: type, haystack: []const T, needle: T) error{NotFound}!T {
+    for (0..haystack.len - 1) |i| {
+        const v = haystack[i];
+        if (v == needle)
+            return v;
+    }
+    return error.NotFound;
+}
+
 inline fn assert(cond: bool) !void {
     if (!cond)
         return error.ParsingFailed;
@@ -182,21 +235,23 @@ inline fn assert(cond: bool) !void {
 const testing = std.testing;
 
 inline fn load(comptime path: []const u8) !Database {
-    return try parse(@embedFile(path));
+    return try parse(testing.allocator, @embedFile(path));
 }
 
 test "should able to parse compiled terminfo" {
-    const db = try load("./tests/st-256color");
-    try testing.expectEqualSlices(u8, db.getStringCapability(.bell).?, "\x07");
+    (try load("./tests/cancer-256color")).deinit(testing.allocator);
 }
 
-// test "should able to get the name of the terminal" {
-//     const db = try parse(@embedFile("./tests/dumb-emacs-ansi"));
-//     try testing.expectEqualSlices(u8, db.name, "dumb-emacs-ansi");
-// }
+test "should able to get the name of the terminal" {
+    const db = try load("./tests/dumb-emacs-ansi");
+    defer db.deinit(testing.allocator);
+    try testing.expectEqualSlices(u8, db.name, "dumb-emacs-ansi");
+}
 
-// test "should able to get the capability of the terminal" {
-//     const db = try parse(@embedFile("./tests/st-256color"));
-//     try testing.expectEqual(db.getNumberCapability(.columns), 80);
-//     try testing.expectEqualSlices(u8, db.getStringCapability(.enter_bold_mode).?, "\\E[1m");
-// }
+test "should able to get the capability of the terminal" {
+    const db = try load("./tests/st-256color");
+    defer db.deinit(testing.allocator);
+    try testing.expectEqual(db.getBooleanCapability(.auto_right_margin), true);
+    try testing.expectEqual(db.getNumberCapability(.columns).?, 80);
+    try testing.expectEqualSlices(u8, db.getStringCapability(.bell).?, "\x07");
+}
